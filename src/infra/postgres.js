1ef3ce29d85e9cf44dbcc74f5722cecce46bf622
Daivik S Gokhale <daiviksg9@gmai.com>
2026-04-15 14:58:01 +0530

    Update Postgres pool export

--- src/infra/postgres.js
+++ src/infra/postgres.js
@@ -1,33 +1,12 @@
 const { Pool } = require("pg");
 
+const connectionString = process.env.DATABASE_URL;
+
 const pool = new Pool({
-  connectionString: "postgres://postgres.lsiafvzsafoaydjrfoeh:%40sJBHS356018111977@aws-1-ap-south-1.pooler.supabase.com:6543/postgres?sslmode=require",
+  connectionString,
   ssl: {
     rejectUnauthorized: false,
-  }
+  },
 });
 
-async function query(text, params) {
-  return pool.query(text, params);
-}
-
-async function withTransaction(work) {
-  const client = await pool.connect();
-  try {
-    await client.query("BEGIN");
-    const result = await work(client);
-    await client.query("COMMIT");
-    return result;
-  } catch (error) {
-    await client.query("ROLLBACK");
-    throw error;
-  } finally {
-    client.release();
-  }
-}
-
-module.exports = {
-  pool,
-  query,
-  withTransaction
-};
+module.exports = pool;
